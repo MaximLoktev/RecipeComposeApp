a13113bb37bcbbea4f88f41751b3bc8fd8f94ae4
DJNooZ <loktevmaksim@yandex.ru>
2026-03-09 23:37:20 +0300

    RCA-04 Organize app structure with Scaffold in MainActivity

--- app/src/main/java/com/example/recipecomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipecomposeapp/MainActivity.kt
@@ -4,8 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,13 +16,15 @@ import com.example.recipecomposeapp.ui.theme.RecipeComposeAppTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
+
         setContent {
-            RecipeComposeAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+            MaterialTheme {
+                Scaffold { paddingValues ->
+                    Text(
+                        text = "Recipes App",
+                        modifier = Modifier.padding(paddingValues)
                     )
                 }
             }
