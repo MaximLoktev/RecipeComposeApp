02cee2b5c6a58c761daefc7ffed54a805f8ef651
DJNooZ <loktevmaksim@yandex.ru>
2026-05-10 16:42:43 +0300

    RCA-31 fetch categories from API with URLConnection and Serialization

--- app/src/main/java/com/example/recipecomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipecomposeapp/MainActivity.kt
@@ -2,12 +2,18 @@ package com.example.recipecomposeapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import com.example.recipecomposeapp.data.model.CategoryDto
+import kotlinx.serialization.json.Json
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.concurrent.thread
 
 class MainActivity : ComponentActivity() {
 
@@ -15,6 +21,34 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Log.d("NetworkTest", "Метод onCreate() выполняется на потоке: ${Thread.currentThread().name}")
+
+        thread {
+            Log.d("NetworkTest", "Выполняю запрос на потоке: ${Thread.currentThread().name}")
+
+            try {
+                val url = URL("https://recipes.androidsprint.ru/api/category")
+
+                val connection = url.openConnection() as HttpURLConnection
+                connection.requestMethod = "GET"
+                connection.connect()
+
+                val responseText = connection.inputStream.bufferedReader().readText()
+
+                Log.d("NetworkTest", "Сырой ответ от сервера:\n$responseText")
+
+                val categories: List<CategoryDto> = Json.decodeFromString(responseText)
+
+                Log.d("NetworkTest", "Количество полученных категорий: ${categories.size}")
+
+                categories.forEach { category ->
+                    Log.d("NetworkTest", "Категория: ${category.title}")
+                }
+            } catch (e: Exception) {
+                Log.e("NetworkTest", "Произошла ошибка при выполнении запроса: ${e.message}", e)
+            }
+        }
 
         enableEdgeToEdge()
 
