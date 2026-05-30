caeeae8ffe273d4b2576d5d1619bbfba1ce8c134
DJNooZ <loktevmaksim@yandex.ru>
2026-05-30 17:56:35 +0300

    RCA-50 fix

--- app/src/androidTest/java/com/example/recipecomposeapp/features/recipes/ui/RecipesContentTest.kt
+++ app/src/androidTest/java/com/example/recipecomposeapp/features/recipes/ui/RecipesContentTest.kt
@@ -4,11 +4,14 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.recipecomposeapp.features.recipes.presentation.model.RecipeUiModel
 import com.example.recipecomposeapp.features.recipes.presentation.model.RecipesUiState
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
 class RecipesContentTest {
 
     @get:Rule
